98fa99937d9167a45278a7cb6e15fb07321fb4f0
sasha <sasha@localhost.localdomain>
2018-11-23 16:57:20 +0300

    sort and filter

--- desktop/app/src/Flies/index.js
+++ desktop/app/src/Flies/index.js
@@ -3,12 +3,44 @@ import FlyIn from "FlyIn";
 import FlyLate from "FlyLate";
 import FlyOut from "FlyOut";
 import FlySearch from "FlySearch";
+import FlyTable from "FlyTable";
 import React, { Component } from "react";
 import sun from "Flies/img/sun.svg";
+
+/**
+ * Get Time in timezone
+ */
+function getTZTime(tzOffset, local = new Date()) {
+  const off = local.getTimezoneOffset() * 60 * 1000;
+  const timeStamp = local.getTime() + off;
+  const nd = new Date();
+  nd.setTime(timeStamp + 1000 * 60 * tzOffset);
+  return nd;
+}
+/**
+ * Format date as YYYY-MM-DD
+ */
+function formatDate(time) {
+  let mm = time.getMonth() + 1;
+  if (mm < 10) mm = `0${mm}`;
+  let dd = time.getDate();
+  if (dd < 10) dd = `0${dd}`;
+  return `${time.getFullYear()}-${mm}-${dd}`;
+}
+
+/**
+ * Format date as YYYY-MM-DD
+ */
+function formatTime(time) {
+  let hh = time.getHours();
+  if (hh < 10) hh = `0${hh}`;
+  let mm = time.getMinutes();
+  if (mm < 10) mm = `0${mm}`;
+  return `${hh}:${mm}`;
+}
 /**
  * Flies list
- * @reactProps {Array} cities List of cities in selector
- * @reactProps {string} city Cty selecetd by default
+ * @reactProps {string} iata Code of airport
  * @reactProps {Function} change Call on change city
  */
 class Flies extends Component {
@@ -21,22 +53,70 @@ class Flies extends Component {
      * @ignore
      */
     this.state = {
-      show: "*",
-      city: props.city,
+      show: "all",
+      iata: props.iata,
       // eslint-disable-next-line
       change: props.change || (e => {console.log(e)}),
-      table: null
+      departure: null,
+      arrival: null,
+      title: "",
+      search: ""
     };
     this.show = this.show.bind(this);
+    this.search = this.search.bind(this);
   }
 
   show(what) {
     return e => {
       this.setState(
-        { show: e.target.checked ? what : "*" },
+        { show: e.target.checked ? what : "all" },
         this.state.change(this.value)
       );
     };
+  }
+
+  search(e) {
+    this.setState({ search: e.target.value, show: "num" });
+  }
+
+  info(type) {
+    if (!this.state[type]) {
+      return [];
+    }
+    return this.state[type].schedule.map(
+      ({
+        [type]: time,
+        thread: { number, title, carrier: { title: carrier__title }, vehicle }
+      }) => ({
+        type,
+        time: new Date(time),
+        number,
+        title,
+        vehicle,
+        carrier__title
+      })
+    );
+  }
+
+  get arrival() {
+    return this.info("arrival");
+  }
+
+  get departure() {
+    return this.info("departure");
+  }
+
+  get num() {
+    // eslint-disable-next-line
+    return this.all.filter(x => x.number.indexOf(this.state.search.toUpperCase()) > -1);
+  }
+
+  get late() {
+    return [];
+  }
+
+  get all() {
+    return this.info("arrival").concat(this.info("departure"));
   }
 
   /**
@@ -44,79 +124,73 @@ class Flies extends Component {
    */
   get value() {
     return {
-      city: this.state.city,
+      iata: this.state.iata,
+      title: this.state.title,
       table: this.state.table,
       show: this.state.show
     };
   }
 
   /**
-   * Handle change of city. Get weather from remote
-   * Call props.change if any
-   */
-  handleChange(e) {
-    const city = e.target.value;
-    this.setState(
-      {
-        city,
-        weather: null,
-        weather_date: null,
-        errors: []
-      },
-      () => {
-        this.state.change(this.value);
-        this.loadCityWeather(city);
-      }
-    );
-  }
-
-  /**
    * Redraw table data on create component
    */
   componentDidMount() {
-    this.loadFlyTable(this.state.city);
+    this.loadFlyTable({ iata: this.state.iata });
   }
 
   /**
    * Load weather from openweathermap.org
    */
-  loadFlyTable(city) {
-    city = "s9600396";
-    clearTimeout(this.state.timeout);
-    this.setState({
-      timeout: setTimeout(() => {
-        this.loadFlyTable();
-      }, 1000 * 60)
-    });
+  loadFlyTable({ iata, evt = "departure" }) {
+    // City = "s9600396";
+    const key = "a5c4937c-81d4-4228-a37b-efa73fe721f9";
+    const date = formatDate(getTZTime(180));
+    if (evt !== "arrival") {
+      clearTimeout(this.state.timeout);
+      setTimeout(() => {
+        this.loadFlyTable({ iata, evt: "arrival" });
+      });
+      this.setState({
+        timeout: setTimeout(() => {
+          this.loadFlyTable({ iata });
+        }, 1000 * 60 * 30)
+      });
+    }
     fetch(
       /* Code: s9600396*/
       /* Station_type: airport*/
       /* Transport_type: plane*/
-      /* `https://api.openweathermap.org/data/2.5/weather?q=${city}&APPID=bd5e378503939ddaee76f12ad7a97608`*/
-      `https://api.rasp.yandex.net/v3.0/schedule/?apikey=dc96082a-c0f3-4699-aaf9-f32908c4a33e&format=json&lang=en_RU&&station=${city}&transport_types=plane`,
-      /* `http://api.rasp.yandex.net/v3.0/schedule/?apikey=27cc38c3-fd6f-4da5-83a6-a2ecb491f985&format=json&lang=en_RU&&station=${city}&transport_types=plane`,*/ {
-        mode: "no-cors"
+      `https://cors.io/?https://api.rasp.yandex.net/v3.0/schedule/?apikey=${key}&format=json&lang=ru_RU&station=${iata}&transport_types=plane&date=${date}&event=${evt}&system=iata&show_systems=all`,
+      {
+        mode: "cors"
       }
     )
       .then(resp => {
         // eslint-disable-next-line
-	console.log('resp',resp);
+        console.log('resp',resp);
         if (resp.ok) {
           return resp.json();
         }
       })
       .then(json => {
+        const { station: { station_type_name, title } } = json;
+
         this.setState(
           {
             table_date: new Date(),
-            table: json
+            [evt]: json,
+            title: `${station_type_name} ${title}`
           },
           () => {
             // eslint-disable-next-line
-	    console.log('val',this.state);
+          console.log('val',this);
             this.state.change(this.value);
           }
         );
+      })
+      .catch(e => {
+        // eslint-disable-next-line
+      console.log('err',e);
       });
   }
 
@@ -127,25 +201,23 @@ class Flies extends Component {
     return (
       <div className="flies">
         <img src={sun} alt="" className="flies__sun" />
+        <h1 className="flies__title">{this.state.title}</h1>
         <div className="flies__buttons">
           <FlyOut
-            onChange={this.show("out")}
-            checked={this.state.show === "out"}
+            onChange={this.show("departure")}
+            checked={this.state.show === "departure"}
           />
           <FlyLate
             onChange={this.show("late")}
             checked={this.state.show === "late"}
           />
           <FlyIn
-            onChange={this.show("in")}
-            checked={this.state.show === "in"}
+            onChange={this.show("arrival")}
+            checked={this.state.show === "arrival"}
           />
         </div>
-        <FlySearch onChange={this.show("number")} />
-        {/* <FlyTable
-          filter={this.state.show}
-          city={this.state.city}
-        /> */}
+        <FlySearch onChange={this.search} />
+        <FlyTable source={this[this.state.show]} formatTime={formatTime} />
       </div>
     );
   }
---
+++ desktop/app/src/FlyTable/index.js
@@ -0,0 +1,48 @@
+import PropTypes from "prop-types";
+import React, { Component } from "react";
+import "FlyTable/css/index.css";
+import i18n from "FlyTable/i18n/index.json";
+/**
+ * FlyTable
+ */
+class FlyTable extends Component {
+  /**
+   * @ignore
+   */
+  render() {
+    return (
+      <table className="flytable">
+        <thead className="flytable__head">
+          <tr>
+            <th className="flytable__th">{i18n.number}</th>
+            <th className="flytable__th">{i18n.time}</th>
+            <th className="flytable__th">{i18n.title}</th>
+            <th className="flytable__th">{i18n.vehicle}</th>
+            <th className="flytable__th">{i18n.carrier__title}</th>
+          </tr>
+        </thead>
+        <tbody>
+          {this.props.source
+            .sort((x, y) => x.time.getTime() - y.time.getTime())
+            .map((x, i) => (
+              <tr key={i} className="flytable__tr">
+                <td className="flytable__td">{x.number}</td>
+                <td className="flytable__td">
+                  {this.props.formatTime(x.time)}
+                </td>
+                <td className="flytable__td">{x.title}</td>
+                <td className="flytable__td">{x.vehicle}</td>
+                <td className="flytable__td">{x.carrier__title}</td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    );
+  }
+}
+FlyTable.propTypes = {
+  source: PropTypes.array,
+  formatTime: PropTypes.func
+};
+FlyTable.defaultProps = {};
+export default FlyTable;
